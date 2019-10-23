1b676591718188d5296c19b762700f69ead5f69c
Christian Schulz <christian.schulz@univie.ac.at>
2019-10-23 16:12:38 +0200

    misc

--- lib/tools/sanity.h
+++ lib/tools/sanity.h
@@ -5,11 +5,12 @@
 #ifndef SANITY_AEH1IUTO
 #define SANITY_AEH1IUTO
 
-void check_matching(dyn_graph_access * G, dyn_matching * algorithm) {
+void check_matching(dyn_graph_access * G, dyn_matching * algorithm, unsigned int matching_size) {
         std::cout << "************ checking matching ****************"  << std::endl;
         // check if matching is really a matching:
         std::vector< NodeID > matchingp = algorithm->getM();
         std::vector< bool > is_matched(G->number_of_nodes(), false);
+        unsigned int internal_matching_size = 0;
         forall_nodes_d((*G), n) {
                 NodeID matching_partner = matchingp[n];
                 if( matching_partner == NOMATE ) {
@@ -23,9 +24,14 @@ void check_matching(dyn_graph_access * G, dyn_matching * algorithm) {
                 if( is_matched[n] || is_matched[matching_partner] ) {
                         std::cout <<  "not a matching, vertex already matched"  << std::endl;
                 }
+
+                internal_matching_size++;
                 is_matched[n] = true;
                 is_matched[matching_partner] = true;
         } endfor
+        if( internal_matching_size != matching_size ) {
+                std::cout <<  "matching size does not match"  << std::endl;
+        }
 }
 
 #endif /* end of include guard: SANITY_AEH1IUTO */
